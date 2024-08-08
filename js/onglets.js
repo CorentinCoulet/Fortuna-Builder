ba469d929403502ba2c69327c4cd4926fc42e0e2
Coco <coulet.corentin@gmail.com>
2024-08-09 00:45:17 +0200

    premier dépot épuré

--- js/onglets.js
+++
@@ -1,9 +0,0 @@
-function showTab(tabId) {
-    const tabs = document.querySelectorAll('.tab');
-    tabs.forEach(tab => {
-        tab.classList.remove('active-tab');
-    });
-
-    const tab = document.getElementById(tabId);
-    tab.classList.add('active-tab');
-}
